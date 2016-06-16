5f6771caf1b2769b1de659fee3ecc6f95f44ca34
paq85 <damian.sromek@gmail.com>
2016-06-16 16:24:41 +0200

    PHP 7.0 and Symfony 3.1

--- Tests/Large/PHPBuiltInWebServerTest.php
+++ Tests/Large/PHPBuiltInWebServerTest.php
@@ -8,10 +8,14 @@ class PHPBuiltInWebServerTest extends \PHPUnit_Framework_TestCase
 {
     public function testItWorks()
     {
-        $server = new PhpBuiltInWebServer('localhost', 8578, __DIR__);
+        $server = new PhpBuiltInWebServer('localhost', 8578, __DIR__.'/../foo_public_html');
+        
         $server->start();
-        $this->assertTrue($server->isRunning());
+        $this->assertTrue($server->isRunning(), 'Server should be running.');
+        
+        $this->assertEquals('It works!', file_get_contents('http://localhost:8578/index.php'), 'Server should be serving content.');
+        
         $server->stop();
-        $this->assertFalse($server->isRunning());
+        $this->assertFalse($server->isRunning(), 'Server should not be running.');
     }
 }
